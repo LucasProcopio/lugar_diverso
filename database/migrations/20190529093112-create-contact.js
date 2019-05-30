48ff35be4e38016ed0d8f21dd48931925e73d589
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-05-30 06:46:11 -0300

    feat: About api server
    Add webservice for the about page

--- database/migrations/20190529093112-create-contact.js
+++ database/migrations/20190529093112-create-contact.js
@@ -8,6 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      about_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
       phone: {
         type: Sequelize.INTEGER
       },
--- src/models/about.js
+++ src/models/about.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   )
   About.associate = function (models) {
     // associations can be defined here
+    About.hasOne(models.Contact)
   }
   return About
 }
